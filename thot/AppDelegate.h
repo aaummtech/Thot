6f5fa3cb36913fdd6d04979c13386fd38759a9ec
Vishnu Vardhan Balakrishnan <vishnu@Vishnu-Vardhan-Balakrishnans-iMac.local>
2014-03-08 23:15:54 +0530

    Initial Commit

---
+++ thot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  thot
+//
+//  Created by Vishnu Vardhan Balakrishnan on 08/03/14.
+//  Copyright (c) 2014 aaummtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ thot/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  thot
+//
+//  Created by Vishnu Vardhan Balakrishnan on 08/03/14.
+//  Copyright (c) 2014 aaummtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
